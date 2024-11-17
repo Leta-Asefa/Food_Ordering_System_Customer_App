16ce203c609833bdb628be83e025213de70903fb
Leta Asefa <letaassefa7@gmail.com>
2024-11-17 17:46:36 +0300

    started

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(){
+
+  return (
+    <View >
+    <Text className='text-white'>What</Text>
+    </View>
+  );
+}
+
+
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3490dc',
+        secondary: '#ffed4a',
+        danger: '#e3342f',
+      },
+    },
+  },
+  plugins: [],
+};
