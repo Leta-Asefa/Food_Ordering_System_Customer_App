ac4592f7433be6e3f7071605b3b1d635b7af804d
Leta Asefa <letaassefa7@gmail.com>
2024-11-23 20:59:09 +0300

    login screen follows the same design rule as sign up screen

--- src/screens/auths/login.js
+++ src/screens/auths/login.js
@@ -1,18 +1,22 @@
 import React, { useState } from 'react'
-import { Text, View, Image, TouchableOpacity, TextInput, TouchableWithoutFeedback, Keyboard, Alert, ToastAndroid } from 'react-native'
+import { Text, View, Image, TouchableOpacity, TextInput, TouchableWithoutFeedback, Keyboard, Alert, ToastAndroid, ImageBackground } from 'react-native'
 import validationSchema from '../../validations/login'
 import api from '../../utilities_and_constants/axiosInstance'
+import backgroud from '../../assets/background.png'
 
 const header = 'text-center text-3xl font-bold mt-14 text-primary'
 const image = 'w-40 h-40 mx-auto rounded-full my-3'
-const textinput = 'bg-gray-300 w-72 mx-auto rounded-lg text-black p-3 mt-3'
-const button = 'w-36 text-xl text-center text-white bg-primary rounded-lg px-3 py-1 mt-3 mx-auto'
-const footer = 'text-orange-950 underline text-center mt-5'
+const textinput = 'mt-3 mb-1 text-orange-500 font-bold'
+const button = 'w-36 text-xl text-center text-white bg-orange-600 rounded-lg px-3 py-1 mt-3 mx-auto'
+const footer = 'text-orange-950 underline text-center mt-2'
 
-export default function Login() {
+export default function Login({ navigation }) {
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [isUsernameFocused, setIsUsernameFocused] = useState(false)
+  const [isPasswordFocused, setIsPasswordFocused] = useState(false)
+
   const [error, setError] = useState([])
 
   const validateInputs = async (inputs) => {
@@ -33,8 +37,8 @@ export default function Login() {
   const handleSubmit = async () => {
     const isValid = await validateInputs({ username, password })
     if (isValid) {
-   //   const response=await api.post('/login',{username,password})
-   //  do the login here
+      //   const response=await api.post('/login',{username,password})
+      //  do the login here
     }
     else {
       ToastAndroid.showWithGravity(error, ToastAndroid.LONG, ToastAndroid.TOP)
@@ -49,43 +53,65 @@ export default function Login() {
 
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
 
-      <View className='bg-gray-200 h-full'>
-        <Text className={header}>Welcom To Zen Audio Books Store</Text>
-       
-        <Image source={require('../../assets/login.jpg')} className={image} />
+      <ImageBackground
+        source={backgroud}
+        className='w-full h-full'
+      >
 
-        <TextInput
-          placeholder="username"
-          placeholderTextColor={'#bfbfbf'}
-          className={textinput}
-          onChangeText={(username) => setUsername(username)}
-        />
+        <View className=' flex-1 flex-col items-center justify-between'>
 
-        <TextInput
-          placeholder="password"
-          placeholderTextColor={'#afafaf'}
-          className={textinput}
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        />
+          <Text
+            className='text-black font-bold text-2xl text-center mt-5'>Welcome To
+            <Text className='text-orange-500'>Let's Eat </Text>Delivery
+          </Text>
 
-        <TouchableOpacity
-          onPress={handleSubmit}>
-          <Text className={button}>Login</Text>
-        </TouchableOpacity>
+          <View className='bg-white opacity-90 rounded-3xl w-full'>
 
-        <TouchableOpacity>
-          <Text className={footer}>create new account ?</Text>
-        </TouchableOpacity>
+            <View className='p-5'>
 
-        <TouchableOpacity>
-          <Text className={footer}>forgot password ?</Text>
-        </TouchableOpacity>
+              <Text className=' text-orange-500 text-xl font-bold text-center'> Login </Text>
 
+              <Text className='mt-3 mb-1 text-orange-500 font-bold'>Username</Text>
+              <TextInput
+                placeholder="username"
+                placeholderTextColor={'#bfbfbf'}
+                className={`rounded-md  ${isUsernameFocused ? 'border-orange-600' : 'border-orange-300'}   border-b-2 text-black p-3'`}
+                onChangeText={(username) => setUsername(username)}
+                onFocus={() => setIsUsernameFocused(true)}
+                onBlur={() => setIsUsernameFocused(false)} />
 
-        <Text className={footer}>all rights are reserved 2024 G.C </Text>
+              <Text className='mt-3 mb-1 text-orange-500 font-bold'>Password</Text>
+              <TextInput
+                placeholder="password"
+                placeholderTextColor={'#afafaf'}
+                className={`rounded-md  ${isPasswordFocused ? 'border-orange-600' : 'border-orange-300'}   border-b-2 text-black p-3'`}
+                secureTextEntry={true}
+                onChangeText={(password) => setPassword(password)}
+                onFocus={() => setIsPasswordFocused(true)}
+                onBlur={() => setIsPasswordFocused(false)}
+              />
 
-      </View>
+              <TouchableOpacity
+                onPress={handleSubmit}
+                className='w-40 mx-auto bg-orange-500 mt-3 rounded-lg p-1'>
+                <Text className='text-center text-white text-2xl font-bold'>Login</Text>
+              </TouchableOpacity>
+
+              
+                <TouchableOpacity onPress={() => navigation.navigate('restaurants_list')}>
+                  <Text className={footer}>create new account ?</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                  <Text className={footer}>forgot password ?</Text>
+                </TouchableOpacity>
+         
+            </View>
+
+          </View>
+
+        </View>
+      </ImageBackground>
 
     </TouchableWithoutFeedback>
 
